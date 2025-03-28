658b48291efaddddb1c67e37415224a86f11790f
Dennis <dennisbircan@gmail.com>
2025-03-28 19:53:15 +0100

    Repo files added

---
+++ Infrastructure/Repositories/BaseRepository.cs
@@ -0,0 +1,58 @@
+﻿using Infrastructure.Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Infrastructure.Repositories
+{
+
+    public interface IBaseRepository<TEntity> where TEntity : class
+    {
+        Task<bool> AddAsync(TEntity entity);
+        Task<bool> DeleteAsync(Expression<Func<TEntity, bool>> expression);
+        Task<bool> ExistsAsync(Expression<Func<TEntity, bool>> expression);
+        Task<IEnumerable<TEntity>> GetAllAsync(bool orderByDescending = false, Expression<Func<TEntity, object>>? sortBy = null, Expression<Func<TEntity, bool>>? filterBy = null, params Expression<Func<TEntity, object>>[] includes);
+        Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> findBy, params Expression<Func<TEntity, object>>[] includes);
+        Task<bool> UpdateAsync(TEntity entity);
+    }
+    public abstract class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : class
+    {
+        protected readonly DataContext _context;
+        protected readonly DbSet<TEntity> _dbSet;
+
+        protected BaseRepository(DataContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<TEntity>();
+        }
+
+        public virtual async Task<bool> AddAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual async Task<bool> DeleteAsync(Expression<Func<TEntity, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual async Task<bool> ExistsAsync(Expression<Func<TEntity, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual async Task<IEnumerable<TEntity>> GetAllAsync(bool orderByDescending = false, Expression<Func<TEntity, object>>? sortBy = null, Expression<Func<TEntity, bool>>? filterBy = null, params Expression<Func<TEntity, object>>[] includes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual async Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> findBy, params Expression<Func<TEntity, object>>[] includes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual async Task<bool> UpdateAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Infrastructure/Repositories/ClientRepository.cs
@@ -0,0 +1,13 @@
+﻿using Infrastructure.Data.Contexts;
+using Infrastructure.Data.Entities;
+
+namespace Infrastructure.Repositories
+{
+    public interface IClientRepository : IBaseRepository<ClientEntity>
+    {
+    }
+
+    public class ClientRepository(DataContext context) : BaseRepository<ClientEntity>(context), IClientRepository
+    {
+    }
+}
