93f2985b7767899d0c49bfc6290b7cfd9afc4e11
Dennis <dennisbircan@gmail.com>
2025-03-28 19:31:00 +0100

    Database added and updated

---
+++ Infrastructure/Data/Entities/StatusEntity.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Infrastructure.Data.Entities
+{
+    public class StatusEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        public string StatusName { get; set; } = null!;
+
+        public virtual ICollection<ProjectEntity> Projects { get; set; } = [];
+    }
+}
--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -1,23 +1,27 @@
+using Infrastructure.Data.Contexts;
+using Microsoft.AspNetCore.Rewrite;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("AlphaDB")));
+
+
 
 var app = builder.Build();
+app.MapOpenApi();
+app.UseSwagger();
+app.UseSwaggerUI(x => x.SwaggerEndpoint("/swagger/v1/swagger.json", "Web API"));
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
+app.UseRewriter(new RewriteOptions().AddRedirect("^$", "swagger"));
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+app.UseCors(x => x.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
 app.MapControllers();
 
 app.Run();
